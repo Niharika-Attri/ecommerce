9226b973b3306eb29a31561f539ac9cc28e326ea
Niharika_Attri <niharikaattri2005@gmail.com>
2024-01-13 10:36:06 +0530

    customerlogin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 const customerModel = require('./model/customer.model')
 require('dotenv').config();
 
@@ -57,4 +58,42 @@ app.post('/customerSignup', async (req,res) => {
             message: 'User already exists'
         })
     }
+})
+
+app.post('/customerlogin', async(req, res) => {
+    const data = req.body
+
+    const email = data.email
+    const password = data.password
+
+    try{
+        const existingUser = await customerModel.findOne({
+            email: email 
+        })
+    
+        if(!existingUser){
+            res.status(400).json({
+                message: 'email not registered, please sign in'
+            })
+        }
+    
+        const passwordMatch = await bcrypt.compare(password, existingUser.password)
+        if(!passwordMatch){
+            res.status(401).json({
+                message: 'authentication failed'
+            })
+        }
+    
+        const token = jwt.sign({customerId: existingUser._id}, 'authsystem', {expiresIn: '1h'})
+        res.status(200).json({
+            token, 
+            message:'Successfully logged in '
+        })
+    }catch(err){
+        res.status(500).json({
+            message: 'login failed',
+            error: err
+        })
+    }
+    
 })
